a40cb7683ab627ce5088bb4fe9d4619b492eab03
ashfame <mail@ashfame.com>
2021-10-20 16:12:33 +0530

    define mock matrix client and engine

--- engine/mocks_test.go
+++ engine/mocks_test.go
@@ -1,5 +1,10 @@
 package engine
 
+import (
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/id"
+)
+
 type mockWorkflowStep struct {
 	impact string
 }
@@ -10,4 +15,43 @@ func (m *mockWorkflowStep) run(payload string, e *engine) string {
 
 func NewMockWorkflowStep(impact string) *mockWorkflowStep {
 	return &mockWorkflowStep{impact: impact}
+}
+
+type mockMatrixClient struct {
+}
+
+func (m *mockMatrixClient) Login(*mautrix.ReqLogin) (*mautrix.RespLogin, error) {
+	// assume the best for mock purposes
+	homeserverInfo := mautrix.HomeserverInfo{BaseURL: ""}
+	identityServerInfo := mautrix.IdentityServerInfo{BaseURL: ""}
+
+	return &mautrix.RespLogin{
+		AccessToken: "XXXX",
+		DeviceID:    "YYYY",
+		UserID:      "1",
+		WellKnown: &mautrix.ClientWellKnown{
+			Homeserver:     homeserverInfo,
+			IdentityServer: identityServerInfo,
+		},
+	}, nil
+}
+
+func (m *mockMatrixClient) SendText(roomID id.RoomID, text string) (*mautrix.RespSendEvent, error) {
+	return &mautrix.RespSendEvent{
+		EventID: "AAAA",
+	}, nil
+}
+
+func (m *mockMatrixClient) Sync() error {
+	return nil
+}
+
+func NewMockMatrixClient() MatrixClient {
+	return &mockMatrixClient{}
+}
+
+func NewMockEngine() *engine {
+	return &engine{
+		client: NewMockMatrixClient(),
+	}
 }
