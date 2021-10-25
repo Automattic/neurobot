575175a60ec72563355ac137cabfcb00ab453276
ashfame <mail@ashfame.com>
2021-10-25 23:21:17 +0530

    draft message to post in matrixworkflowstep properly

--- engine/workflow_step_matrix_post_message.go
+++ engine/workflow_step_matrix_post_message.go
@@ -17,8 +17,15 @@ type postMessageMatrixWorkflowStep struct {
 }
 
 func (s postMessageMatrixWorkflowStep) run(payload string, e *engine) (string, error) {
+	msg := payload
 	// Append message specified in definition of this step as a prefix to the payload
-	msg := fmt.Sprintf("%s\n%s", s.message, payload)
+	if s.message != "" {
+		if payload != "" {
+			msg = fmt.Sprintf("%s %s", s.message, payload)
+		} else {
+			msg = s.message
+		}
+	}
 	_, err := e.client.SendText(id.RoomID(s.room), msg)
 	if err != nil {
 		fmt.Println(err)
