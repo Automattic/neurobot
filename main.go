341494f642a92f5889d27cd74ff3ea667d65422d
Ashfame <mail@ashfame.com>
2022-01-04 07:09:01 +0400

    provision a static file server for UI

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"matrix-workflow-builder/engine"
+	"net/http"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"maunium.net/go/mautrix"
@@ -35,6 +37,7 @@ func main() {
 	username := os.Getenv("MATRIX_USERNAME")
 	password := os.Getenv("MATRIX_PASSWORD")
 	webhookListenerPort := os.Getenv("WEBHOOK_LISTENER_PORT")
+	staticServerPort := os.Getenv("STATIC_SERVER_PORT")
 
 	// if either one matrix related env var is specified, make sure all of them are specified
 	isMatrix := false
@@ -46,34 +49,60 @@ func main() {
 		}
 	}
 
-	// set default port for running webhook listener server
+	// set default ports for running servers, if not provided
 	if webhookListenerPort == "" {
 		webhookListenerPort = "8080"
 	}
-
-	p := engine.RunParams{
-		Debug:               debug,
-		Database:            *dbFile,
-		PortWebhookListener: webhookListenerPort,
-		IsMatrix:            isMatrix,
-		MatrixHomeServer:    homeserver,
-		MatrixUsername:      username,
-		MatrixPassword:      password,
+	if staticServerPort == "" {
+		staticServerPort = "8181"
 	}
 
-	e := engine.NewEngine(p)
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
 
-	if isMatrix {
-		mc, err := mautrix.NewClient(homeserver, "", "")
-		if err != nil {
-			log.Fatal(err)
+	// Run static server for UI
+	go func() {
+		mux := http.NewServeMux()
+		mux.Handle("/", http.FileServer(http.Dir("./ui")))
+
+		log.Println("Starting static file server on :" + staticServerPort)
+		staticServer := http.Server{
+			Addr:    fmt.Sprintf(":%v", staticServerPort),
+			Handler: mux,
 		}
-		e.StartUp(mc, mc.Syncer.(*mautrix.DefaultSyncer))
-	} else {
-		fmt.Println("engine:", "Lite mode")
-		e.StartUpLite()
-	}
-	defer e.ShutDown()
+		log.Fatal(staticServer.ListenAndServe())
+		wg.Done()
+	}()
 
-	e.Run()
+	// Run Workflow builder engine
+	go func() {
+		p := engine.RunParams{
+			Debug:               debug,
+			Database:            *dbFile,
+			PortWebhookListener: webhookListenerPort,
+			IsMatrix:            isMatrix,
+			MatrixHomeServer:    homeserver,
+			MatrixUsername:      username,
+			MatrixPassword:      password,
+		}
+
+		e := engine.NewEngine(p)
+
+		if isMatrix {
+			mc, err := mautrix.NewClient(homeserver, "", "")
+			if err != nil {
+				log.Fatal(err)
+			}
+			e.StartUp(mc, mc.Syncer.(*mautrix.DefaultSyncer))
+		} else {
+			fmt.Println("engine:", "Lite mode")
+			e.StartUpLite()
+		}
+		defer e.ShutDown()
+
+		e.Run()
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
