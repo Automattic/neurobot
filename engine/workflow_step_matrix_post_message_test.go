5022def399c8517a0911b22fe14d0566a9f52520
Ashfame <mail@ashfame.com>
2022-02-16 09:53:37 +0400

    fix TestPostMessageMatrixWorkflowStep

--- engine/workflow_step_matrix_post_message_test.go
+++ engine/workflow_step_matrix_post_message_test.go
@@ -243,6 +243,8 @@ func TestPostMessageMatrixWorkflowStep(t *testing.T) {
 		e := NewMockEngine()
 		e.db = dbs
 		e.matrixhomeserver = table.homeserver
+		e.bots = make(map[uint64]MatrixClient)
+		e.bots[1] = botMatrixClient
 
 		s := &postMessageMatrixWorkflowStep{
 			workflowStep: workflowStep{
