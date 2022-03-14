a0bb219ca73aa26dd4920181fe8d85767f9dc634
Paulo Pinto <paulo.pinto@automattic.com>
2022-03-14 16:28:07 +0000

    Implement room.Id

---
+++ domain/room/id.go
@@ -0,0 +1,45 @@
+package room
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Id interface {
+	Id() string
+	IsAlias() bool
+}
+
+type id struct {
+	value string
+}
+
+func NewId(value string) (Id, error) {
+	if value == "" {
+		return nil, errors.New("room id must not be empty")
+	}
+
+	if !strings.HasPrefix(value, "!") && !strings.HasPrefix(value, "#") {
+		return nil, fmt.Errorf("room id must start with ! or #, got %s", value)
+	}
+
+	if !strings.Contains(value, ":") {
+		return nil, fmt.Errorf("room id is missing colon, got %s", value)
+	}
+
+	s := strings.Split(value, ":")
+	if len(s) == 0 || s[0] == "" || s[1] == "" {
+		return nil, fmt.Errorf("room id must have format !foo:example.com or #foo:example.com, got %s", value)
+	}
+
+	return id{value: value}, nil
+}
+
+func (id id) Id() string {
+	return id.value
+}
+
+func (id id) IsAlias() bool {
+	return strings.HasPrefix(id.value, "#")
+}
---
+++ domain/room/id_test.go
@@ -0,0 +1,53 @@
+package room
+
+import (
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	_, err := NewId("")
+	if err == nil {
+		t.Error("must not accept empty string")
+	}
+}
+
+func TestInvalidStartCharacter(t *testing.T) {
+	_, err := NewId("a:matrix.test")
+	if err == nil {
+		t.Errorf("must start with # or !")
+	}
+}
+
+func TestInvalid(t *testing.T) {
+	_, err := NewId("!room")
+	if err == nil {
+		t.Error("must have a colon followed by a hostname")
+	}
+
+	_, err = NewId("!room:")
+	if err == nil {
+		t.Error("must have a hostname")
+	}
+}
+
+func TestValid(t *testing.T) {
+	_, err := NewId("!room:matrix.test")
+	if err != nil {
+		t.Errorf("id is valid so there should be no error, got %s", err)
+	}
+}
+
+func TestId(t *testing.T) {
+	id, _ := NewId("!room:matrix.test")
+	if id.Id() != "!room:matrix.test" {
+		t.Error("invalid id")
+	}
+}
+
+func TestIsAlias(t *testing.T) {
+	id, _ := NewId("#room:matrix.test")
+
+	if !id.IsAlias() {
+		t.Error("id should be an alias")
+	}
+}
