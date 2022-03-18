2770edde9199253f87cedff2f351e02178eef19e
Paulo Pinto <paulo.pinto@automattic.com>
2022-03-18 14:10:54 +0000

    Add function to migrate database

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -1,14 +1,56 @@
 package database
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/upper/db/v4"
 	"github.com/upper/db/v4/adapter/sqlite"
 	"os"
+	"path/filepath"
+	"runtime"
 )
+
+// The URL to the directory containing migrations
+var migrationsUrl string
+
+func init() {
+	_, currentFile, _, _ := runtime.Caller(0)
+	currentDirectory := filepath.Dir(currentFile)
+
+	migrationsUrl = fmt.Sprintf("file://%s/migration/", currentDirectory)
+}
 
 func MakeDatabaseSession() (db.Session, error) {
 	databasePath := os.Getenv("DB_FILE")
 	settings := sqlite.ConnectionURL{Database: databasePath}
 
 	return sqlite.Open(settings)
+}
+
+func Migrate(session db.Session) error {
+	database, err := sql.Open("sqlite3", session.ConnectionURL().String())
+	if err != nil {
+		return err
+	}
+	defer database.Close()
+
+	driver, err := sqlite3.WithInstance(database, &sqlite3.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(migrationsUrl, "sqlite3", driver)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
 }
