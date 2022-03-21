5814853dec1d6b077a2fed5e8b9289aaf0757c8e
Paulo Pinto <paulo.pinto@automattic.com>
2022-03-21 16:12:23 +0000

    Implement FindActive() in bot repository

--- app/bot/repository.go
+++ app/bot/repository.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"github.com/upper/db/v4"
+	model "neurobot/model/bot"
 )
 
 type Repository struct {
@@ -12,4 +13,10 @@ func NewRepository(session db.Session) *Repository {
 	return &Repository{
 		collection: session.Collection("bots"),
 	}
+}
+
+func (repository *Repository) FindActive() (bots []model.Bot, err error) {
+	result := repository.collection.Find(db.Cond{"active": 1})
+	err = result.All(&bots)
+	return
 }
--- model/bot/repository.go
+++ model/bot/repository.go
@@ -2,4 +2,6 @@ package bot
 
 // Repository facilitates persistence and retrieval of bots.
 type Repository interface {
+	// FindActive retrieves all active bots.
+	FindActive() ([]Bot, error)
 }
