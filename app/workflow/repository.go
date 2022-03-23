e473e541696472d163ab0c91cd0fde700ac06606
Paulo Pinto <paulo.pinto@automattic.com>
2022-03-23 14:08:54 +0000

    Don't export repository

--- app/workflow/repository.go
+++ app/workflow/repository.go
@@ -14,19 +14,19 @@ type meta struct {
 	Value      string `db:"value"`
 }
 
-type Repository struct {
+type repository struct {
 	collection     db.Collection
 	collectionMeta db.Collection
 }
 
-func NewRepository(session db.Session) *Repository {
-	return &Repository{
+func NewRepository(session db.Session) *repository {
+	return &repository{
 		collection:     session.Collection("workflows"),
 		collectionMeta: session.Collection("workflow_meta"),
 	}
 }
 
-func (repository *Repository) FindActive() (workflows []model.Workflow, err error) {
+func (repository *repository) FindActive() (workflows []model.Workflow, err error) {
 	result := repository.collection.Find(db.Cond{"active": 1})
 	if err := result.All(&workflows); err != nil {
 		return nil, err
@@ -42,7 +42,7 @@ func (repository *Repository) FindActive() (workflows []model.Workflow, err erro
 }
 
 // Load information from workflow_meta table into a workflow object.
-func (repository *Repository) loadMeta(workflow *model.Workflow) (err error) {
+func (repository *repository) loadMeta(workflow *model.Workflow) (err error) {
 	var metas []meta
 	result := repository.collectionMeta.Find(db.Cond{"workflow_id": workflow.ID})
 	if err := result.All(&metas); err != nil {
