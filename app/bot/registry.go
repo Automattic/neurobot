bcb58c8ff3854f5b968975cfd6cb8ed5b5b613ce
Paulo Pinto <paulo.pinto@automattic.com>
2022-03-30 16:48:46 +0100

    Implement Append() and GetClient() in bot registry

--- app/bot/registry.go
+++ app/bot/registry.go
@@ -1,10 +1,14 @@
 package bot
 
 import (
+	"fmt"
 	"neurobot/infrastructure/matrix"
+	model "neurobot/model/bot"
 )
 
 type Registry interface {
+	Append(bot model.Bot, client matrix.Client) error
+	GetClient(identifier string) matrix.Client
 }
 
 type registry struct {
@@ -14,4 +18,22 @@ type registry struct {
 
 func NewRegistry(homeserverURL string) *registry {
 	return &registry{homeserverURL: homeserverURL}
+}
+
+func (r *registry) Append(bot model.Bot, client matrix.Client) (err error) {
+	if _, ok := r.clients[bot.Identifier]; ok {
+		return fmt.Errorf("bot %s is already known", bot.Identifier)
+	}
+
+	r.clients[bot.Identifier] = client
+
+	return
+}
+
+func (r *registry) GetClient(identifier string) matrix.Client {
+	if client, ok := r.clients[identifier]; ok {
+		return client
+	}
+
+	return nil
 }
