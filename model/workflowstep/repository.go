4845e38755ffaf075eec58bf2e6f8d76c0d0e67b
Ashfame <mail@ashfame.com>
2022-03-31 10:59:15 +0400

    define repository interface methods for workflow step repo

--- model/workflowstep/repository.go
+++ model/workflowstep/repository.go
@@ -2,5 +2,12 @@ package workflowstep
 
 // Repository facilitates persistence and retrieval of workflow steps.
 type Repository interface {
+	// Save persists a workflow step
 	Save(workflowStep *WorkflowStep) error
+
+	// FindActive retrieves all active workflow steps
+	FindActive() ([]WorkflowStep, error)
+
+	// FindByID retrieves a workflow step by its ID.
+	FindByID(ID uint64) (WorkflowStep, error)
 }
