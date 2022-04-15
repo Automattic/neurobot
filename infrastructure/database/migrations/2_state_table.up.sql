bbb8de3238775e9f508668d804b9259c0fec2f97
Ashfame <mail@ashfame.com>
2022-04-15 13:28:16 +0400

    allow values to be zero length strings

--- infrastructure/database/migrations/2_state_table.up.sql
+++ infrastructure/database/migrations/2_state_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE "matrix_state" (
 "bot_id" INTEGER NOT NULL CHECK (bot_id <> ''),
 "what"   TEXT NOT NULL CHECK(what <> ''),
 "id"     TEXT NOT NULL CHECK(id <> ''),
-"value"  TEXT NOT NULL CHECK(value <> '')
+"value"  TEXT NOT NULL
 );
